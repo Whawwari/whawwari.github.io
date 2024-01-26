e75d902e24ed80a5aead62a697bc587b4a558ffb
Whawwari <waleedhawwari.1224@gmail.com>
2024-01-26 17:43:19 -0500

    fifth try

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/whawwari.github.io",
+  base: "/whawwari.github.io/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}]
